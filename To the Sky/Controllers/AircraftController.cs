495ca3e2bed81445696e126eba1e5351a9ced375
zing411 <matthewlu231124>
2026-06-16 14:34:01 -0400

    some models and controllers

---
+++ To the Sky/Controllers/AircraftController.cs
@@ -0,0 +1,19 @@
+﻿using To_the_Sky.Models;
+
+namespace To_the_Sky.Controllers;
+
+
+
+
+public class AircraftController
+{
+    public IEnumerable<AircraftModel> AircraftList { get; set; }
+    
+}
+
+
+// Getting the specific aircraft displayed
+public class AircraftModel
+{
+    public string Id { get; set; }
+}
---
+++ To the Sky/Models/AircraftModel.cs
@@ -0,0 +1,30 @@
+﻿namespace To_the_Sky.Models;
+
+public class AircraftModel
+{
+    
+    // ID of the aircraft
+    public int Id { get; set; }
+    
+    // Name of the aircraft
+    public string Name { get; set; }
+    
+    // Manufacturer of the aircraft
+    public string Manufacturer { get; set; }
+    
+    // the country of origin 
+    public string Country { get; set; }
+    
+    // first flight
+    public string FirstFlightNumber { get; set; }
+    
+    // last flight if applicable  
+    public string LastFlightNumber { get; set; }
+    
+    // description of the aircraft
+    public string Description { get; set; }
+    
+    // image of aircraft
+    public string ImageUrl { get; set; }
+    
+}
